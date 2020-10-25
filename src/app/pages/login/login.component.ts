75de074821e29d9c1f0c26daeabdb34740191d10
eduardoRuizC <764539@unizar.es>
2020-10-25 17:38:31 +0100

    Modificada la ventana de iniciar sesión de la plantilla

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,37 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+  loginData: FormGroup;
+  triedLogin: Boolean;
+
+  constructor() {
+    this.triedLogin = false;
+    this.loginData =  new FormGroup({
+      'user' : new FormControl('', [Validators.required]),
+      'password' : new FormControl('', [Validators.required])
+    });
+  }
 
   ngOnInit() {
+
   }
   ngOnDestroy() {
   }
+
+
+  private login() {
+    const userInput = document.getElementById('userInput');
+    const passwordInput = document.getElementById('passwordInput');
+    this.triedLogin = true;
+    if (this.loginData.get('user').value === '') {
+      userInput.style.border = 'solid #dc3545';
+    }
+    if (this.loginData.get('password').value === '') {
+       passwordInput.style.border = 'solid #dc3545';
+    }
+    alert(this.loginData.get('user').value);
+    alert(this.loginData.get('password').value);
+  }
+
 
 }

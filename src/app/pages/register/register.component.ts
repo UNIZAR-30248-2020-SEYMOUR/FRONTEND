b25dbb848250f92dd1ffe4bc8a5004861c3acada
764429 <764429@unizar.es>
2020-10-25 17:27:52 +0100

    Se ha adaptado la ventana de registro de la plantilla.

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  triying: boolean;
+  private builder: FormBuilder;
+  constructor() {
+    this.triying = false;
+    this.registerForm = new FormGroup({
+      'nickname': new FormControl('', [
+        Validators.required,
+        Validators.minLength(10)
+      ]),
+      'email': new FormControl('', [Validators.required ]),
+      'pswd': new FormControl('', [Validators.required, Validators.minLength(10)]),
+      'repeat_pswd': new FormControl('', [Validators.required]),
+    });
+  }
+
+  checkPasswords() { // here we have the 'passwords' group
+    const pass = this.registerForm.get('pswd').value;
+    const confirmPass = this.registerForm.get('repeat_pswd').value;
+    return pass === confirmPass;
+  }
+
+  submit() {
+    this.triying = true;
+    if (this.registerForm.valid && this.checkPasswords()) {
+      alert('si');
+    }
+
+    console.log(this.registerForm.value);
+    console.log(this.registerForm );
+  }
+
+
 
   ngOnInit() {
   }
-
+  ngOnDestroy() {
+  }
 }
+

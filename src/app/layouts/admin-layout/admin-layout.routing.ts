b1ea09e3ab77da744892d9ae7f4ea7b2c9e05bfd
eduardoRuizC <764539@unizar.es>
2020-11-22 11:14:40 +0100

    Realizada GUI y peticiones de visualizar curso del usuario.

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -6,6 +6,7 @@ import { MapsComponent } from '../../pages/maps/maps.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
 import {ProfileSearchComponent} from '../../pages/profile-search/profile-search.component';
+import {ViewCourseComponent} from '../../pages/view-course/view-course.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
@@ -13,5 +14,6 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'tables',         component: TablesComponent },
     { path: 'icons',          component: IconsComponent },
     { path: 'maps',           component: MapsComponent },
-    { path: 'search-profile/:text', component: ProfileSearchComponent }
+    { path: 'search-profile/:text', component: ProfileSearchComponent },
+    { path: 'view-course/:courseId', component: ViewCourseComponent }
 ];
--- src/app/pages/view-course/view-course.component.ts
+++ src/app/pages/view-course/view-course.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
+import {Course, Video} from '../../interfaces';
+import {CourseService} from '../../services/course.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-view-course',
   templateUrl: './view-course.component.html',
-  styleUrls: ['./view-course.component.css']
+  styleUrls: ['./view-course.component.scss']
 })
 export class ViewCourseComponent implements OnInit {
+  NUM_GET_VIDEOS = 10;
 
-  constructor() { }
+  course: Course;
+  videos: Array<Video>;
+  moreVideos: boolean;
 
-  ngOnInit(): void {
+  constructor(private courseService: CourseService, private route: ActivatedRoute) {
+    const sub = this.route.params.subscribe(params => {
+      this.course = {
+        id: params.courseId,
+        coursename: '',
+        category:  {name: '', imageUrl: ''},
+        description: ''
+      };
+    });
+    this.videos = [];
+    this.moreVideos = true;
+    this.getCourseData();
+    this.getMoreVideos();
   }
 
+  ngOnInit(): void {
+
+  }
+
+  /**
+   * This method get videos of the course that is showing
+   */
+  getMoreVideos() {
+    const observer = this.courseService.getVideos(this.course.id, this.videos.length, (this.videos.length + this.NUM_GET_VIDEOS));
+    observer.subscribe(
+      data => this.showVideos(data.videos),
+      error => {
+        console.log(error.status);
+      }
+    );
+  }
+
+  openCreateVideoPopup() {
+
+  }
+
+  openEditCoursePopup() {
+
+  }
+
+  /**
+   * Add new videos to show in the GUI
+   * @param videos: list of videos to add
+   * @private
+   */
+  private showVideos(videos: Array<Video>) {
+    if (videos.length < this.NUM_GET_VIDEOS) {
+      this.moreVideos = false;
+    }
+    videos.forEach(video => this.videos.push(video));
+    this.moreVideos = true;
+  }
+
+  private getCourseData() {
+    const observer = this.courseService.getCourseData(this.course.id);
+    observer.subscribe(
+      data => {this.course = {
+        id: this.course.id,
+        coursename: data.name,
+        description: data.description,
+        category: data.category
+      }; },
+      error => {console.log(error.status);}
+    );
+  }
 }

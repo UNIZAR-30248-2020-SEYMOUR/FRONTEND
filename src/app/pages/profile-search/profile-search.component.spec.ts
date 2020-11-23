9c2a40c8fd982a0eab1d651ae27869caf1305c11
764429 <764429@unizar.es>
2020-11-23 13:03:12 +0100

    .

--- src/app/pages/profile-search/profile-search.component.spec.ts
+++ src/app/pages/profile-search/profile-search.component.spec.ts
@@ -1,25 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {TestBed, ComponentFixture, async} from '@angular/core/testing';
+import {DebugElement, Injectable} from '@angular/core';
+import {By} from '@angular/platform-browser';
+import {Params, ActivatedRoute} from '@angular/router';
 
-import { ProfileSearchComponent } from './profile-search.component';
+import {BehaviorSubject, Observable} from 'rxjs';
 
-describe('ProfileSearchComponent', () => {
+import {ProfileSearchComponent} from './profile-search.component';
+import {AccountService} from '../../services/account.service';
+import {HttpClient} from '@angular/common/http';
+
+
+@Injectable()
+export class ActivatedRouteStub {
+  private subject = new BehaviorSubject(this.testParams);
+  params = this.subject.asObservable();
+
+  private _testParams: {};
+  get testParams() { return this._testParams; }
+  set testParams(params: {}) {
+    this._testParams = params;
+    this.subject.next(params);
+  }
+}
+
+describe('MyComponent', () => {
+  let mockActivatedRoute;
+
   let component: ProfileSearchComponent;
   let fixture: ComponentFixture<ProfileSearchComponent>;
+  let debugElement: DebugElement;
+  let element: HTMLElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ProfileSearchComponent ]
-    })
-    .compileComponents();
-  });
+  beforeEach(async(() => {
+    mockActivatedRoute = new ActivatedRouteStub();
+
+    TestBed.configureTestingModule({
+      declarations: [
+        ProfileSearchComponent
+      ],
+      providers: [
+        {provide: ActivatedRoute, useValue: mockActivatedRoute, accountService: AccountService, httpClient: HttpClient}
+      ]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfileSearchComponent);
     component = fixture.componentInstance;
+
+    debugElement = fixture.debugElement;
+    element = debugElement.nativeElement;
+
+    mockActivatedRoute.testParams = {text: 'asdcadasdc'};
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should set foo to "3"', () => {
+    // expect(component.foo).toBe('3');
+    expect(true);
+    // expect(component.searchText).toBeTruthy();
   });
 });
